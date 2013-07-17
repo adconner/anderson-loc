4c8caab9cff9972a91314abd9514a68f81c52407
Austin Conner <aconner.vu@gmail.com>
2013-07-17 01:30:36 -0500

    clean up anderson.hs

--- anderson.hs
+++ anderson.hs
@@ -1,5 +1,6 @@
 module Anderson where
 
+import Data.List(unfoldr)
 import Data.Vector.Unboxed (Vector,(!))
 import qualified Data.Vector.Unboxed as V
 
@@ -15,7 +16,7 @@ delta g n = V.generate (numV g) (\i -> if i == n then 1 else 0)
 intensity s = (normalize s !)
   where 
     normalize :: StateV -> Vector Double
-    normalize v = V.map (/ (sqrt . l2norm2 $ v)) v
+    normalize v = V.map (/ l2norm v) v
 
 (+^) = V.zipWith (+)
 neg = V.map negate
@@ -28,6 +29,9 @@ dotp = curry $ V.foldl' (+) 0 . uncurry (V.zipWith (*))
 l2norm2 :: StateV -> Energy
 l2norm2 = V.foldl' (+) 0 . V.map (^2)
 
+l2norm :: StateV -> Energy
+l2norm = sqrt . l2norm2
+
 subtractProj :: StateV -> StateV -> StateV
 subtractProj v w = if l2norm2 w < 0.001 
                     then error $ "tried to subtract the projection onto short vector " ++ show w 
@@ -36,13 +40,13 @@ subtractProj v w = if l2norm2 w < 0.001
 -- subtractProjOrth :: StateV -> StateV -> StateV
 -- subtractProjOrth v w = v -^ ((v `dotp` w) *^ w)
 
-grahamschmidt :: [StateV] -> StateV -> StateV
-grahamschmidt [] s = s
-grahamschmidt (x:xs) s = grahamschmidt xs (s `subtractProj` x)
+successivegs :: StateV -> [StateV] -> [StateV]
+successivegs = scanl subtractProj
 
 -- rewrite to avoid nested data parallelism
 dso :: Adj -> StateV -> StateV
 dso g s = s +^ V.imap (\n e -> 
+-- dso g s = V.imap (\n e -> 
   V.sum (V.map (s !) (neighbors g n)) - fromIntegral (degree g n) * e) s
 
 dsoSimp :: Adj -> StateV -> StateV
@@ -55,13 +59,11 @@ drsoIterate :: Adj -> RandomV -> StateV -> [StateV]
 drsoIterate g r = iterate (drso g r)
 
 drsoIterateGraham :: Adj -> RandomV -> StateV -> [StateV]
-drsoIterateGraham g r s = s : drsoIterate' g r [s]
+drsoIterateGraham g r s = unfoldr (\ss -> Just (head ss, next ss : ss)) [s]
   where
-    drsoIterate' g r ss = next : drsoIterate' g r (next : ss)
-      where 
-        next = grahamschmidt ss (drso g r (head ss))
+    next ss = last $ successivegs (drso g r (head ss)) ss
 
-drsoDists :: [StateV] -> StateV -> [Double]
-drsoDists [] _ = []
-drsoDists (x:xs) s = sqrt (l2norm2 normal) : drsoDists xs normal
-  where normal = subtractProj s x
+drsoNormals d1 g r s = successivegs d1 $ drsoIterate g r s
+
+drsoDists :: StateV -> [StateV] -> [Double]
+drsoDists = curry (map l2norm . uncurry successivegs)
