c65c22d90dac282850708e9623506cafeee356d2
Austin Conner <aconner.vu@gmail.com>
2013-07-23 13:42:53 -0500

    draw now does main computation

--- draw.hs
+++ draw.hs
@@ -6,6 +6,7 @@ import System.Environment
 import System.IO
 import System.Random
 import System.Console.GetOpt
+import Numeric
 
 import Anderson.Norm
 import Anderson.Random
@@ -13,15 +14,15 @@ import Graph
 import Graph.Gasket
 -- import Graph.Ring
 
-data Render = Color | Label
-data Mode = Iterates | Differences Int
+data Render = None | Color | Label deriving Eq
+data Mode = Iterates | Differences Int deriving Eq
 data Options = Options {
   m, n, d0, ntosub :: Int,
   c :: Double,
   mode :: Mode,
   rend :: Render,
   file :: String
-}
+} deriving Eq
 
 defaults = Options {
   m = 5,
@@ -30,7 +31,7 @@ defaults = Options {
   ntosub = 2,
   c = 0,
   mode = Iterates,
-  rend = Color,
+  rend = None,
   file = "pics/graph"
 }
 
@@ -42,20 +43,20 @@ render opts = do
     then return (norandom g) 
     else liftM (genrandom g (c opts)) getStdGen
 
-  let states = case mode opts of {
-    Iterates -> take (n opts + 1) $ drsoIterateGraham (ntosub opts) g r (delta g (d0 opts));
-    Differences d1 -> take (n opts + 1) $ drsoNormals (ntosub opts) (delta g d1) g r (delta g (d0 opts))
-  }
+  let states = case mode opts of
+        Iterates -> take (n opts + 1) $ drsoIterateGraham (ntosub opts) g r (delta g (d0 opts))
+        Differences d1 -> take (n opts + 1) $ drsoNormals (ntosub opts) (delta g d1) g r (delta g (d0 opts))
 
   let padTo = length $ show (n opts)
 
   mapM_ (\(i,s) -> do 
-    putStrLn $ "writing " ++ show i ++ "th state"
+    putStrLn $ show i ++ " " ++ showFFloat (Just 16) (l2norm s) ""
     let is = show i
-    writeFile (file opts ++ "-" ++ replicate (padTo - length is) '0' ++ is ++ ".gv") $
-      case rend opts of
-        Color -> (graphvizColorShow (labelBy r) greenred (intensity s) g)
-        Label -> (graphvizShow (labelBy s) g)) 
+    let todisk = writeFile (file opts ++ "-" ++ replicate (padTo - length is) '0' ++ is ++ ".gv")
+    case rend opts of
+      None -> return ()
+      Color -> todisk $ (graphvizColorShow (labelBy r) greenred (intensity s) g)
+      Label -> todisk $ (graphvizShow (labelBy s) g))
         (zip [0..] states)
   putStrLn "done"
 
@@ -63,29 +64,32 @@ options :: [OptDescr (Options -> Options)]
 options =
   [ Option ['n'] ["iterations"]
       (OptArg (maybe id (\ arg opt -> opt {n = read arg})) "n")
-      "number of iterations, graphs generated will be one more than this value"
+      ("number of iterations, graphs generated will be one more than this value (default: " ++ show (n defaults) ++ ")")
   , Option ['m'] ["graph-size"]
       (OptArg (maybe id (\ arg opt -> opt {m = read arg})) "size")
-      "graph size to use (graph dependant)"
+      ("graph size to use (graph dependant) (default: " ++ show (m defaults) ++ ")")
   , Option ['c'] ["random"]
       (OptArg (maybe id (\ arg opt -> opt {c = read arg})) "bound")
-      "Discrete schodinger operator randomness bound" 
+      ("Discrete schodinger operator randomness bound (default: " ++ show (c defaults) ++ ")")
   , Option ['0'] ["d0"]
       (OptArg (maybe id (\ arg opt -> opt {d0 = read arg})) "i")
-      "vector to start iteration with, expressed as a delta index"
+      ("vector to start iteration with, expressed as a delta index (default: " ++ show (d0 defaults) ++ ")")
   , Option ['1'] ["d1"]
       (OptArg (maybe id (\ arg opt -> opt {mode = Differences (read arg)})) "i")
-      "vector to compare iterated subspaces to, expressed as a delta index."
+      ("vector to compare iterated subspaces to, expressed as a delta index. (default: none)")
   , Option ['N'] ["ntosub"]
       (OptArg (maybe id (\ arg opt -> opt {ntosub = read arg})) "num")
-      "Number of previous state vectors to subtract out by after computing the new state" 
+      ("Number of previous states to Graham Schmidt out by after computing new state (default: " 
+        ++ show (ntosub defaults) ++ ")")
   , Option ['f'] ["fbase"] 
       (OptArg (maybe id (\ arg opt -> opt {file = arg})) "FILE")
-      "basename for generated graphs" 
+      ("basename for generated graphs (default: " ++ file defaults ++ ")")
   , Option ['C'] ["color"] (NoArg (\ opt -> opt {rend = Color}))
-      "Render graph values as variated colors"
+      ("Render graph values as variated colors" ++ if rend defaults == Color then " (default)" else "")
   , Option ['L'] ["label"] (NoArg (\ opt -> opt {rend = Label}))
-      "Render graph values as vertex labels"
+      ("Render graph values as vertex labels" ++ if rend defaults == Label then " (default)" else "")
+  , Option [] ["no-render"] (NoArg (\ opt -> opt {rend = None}))
+      ("Do not render graphs" ++ if rend defaults == None then " (default)" else "")
   ]
     
 getOpts :: [String] -> Options
