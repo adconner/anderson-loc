c440507a454dea4e0ad3413bf6df1dd05e4831e8
Austin Conner <aconner.vu@gmail.com>
2013-07-26 16:01:27 -0500

    fix lattice bug

--- lattice.hs
+++ lattice.hs
@@ -6,7 +6,7 @@ defaultdim = 2
   
 lattice :: Int -> Int -> [([Int], [[Int]])]
 lattice 0 _ = [([], [])] -- (mid, [(vertex, neighbors)])
-lattice dim size = [(s:v, (s-1:v):(s+1:v):n) | s <- [-size..size], (v, n) <- adj]
+lattice dim size = [(s:v, (s-1:v):(s+1:v):map (s:) n) | s <- [-size..size], (v, n) <- adj]
   where adj = lattice (dim-1) size
 
 graph :: Int -> Adj
